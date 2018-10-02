e94e387276fc1bbaf0418109ce8a7880dfaa9ac8
VanGoghDev <kifirigor@gmail.com>
2018-10-02 22:42:08 +0300

    Version 1.0 (first) Program converts binary file into txt file, replacing random symbols with 0 and 1

---
+++ src/ru/firsov/kirill/BaseBinToTxt.java
@@ -0,0 +1,43 @@
+package ru.firsov.kirill;
+
+import java.io.*;
+
+public class BaseBinToTxt {
+
+    private File fileIn;
+    private File fileOut;
+
+    byte[] aByte; //an array where data from file stored
+
+    /**
+     * @param fileIn file to read
+     * @param fileOut file to write
+     */
+    public BaseBinToTxt(File fileIn, File fileOut) {
+        this.fileIn = fileIn;
+        this.fileOut = fileOut;
+    }
+
+    protected void read() {
+        try (DataInputStream input = new DataInputStream(new FileInputStream(this.fileIn))) {
+
+            int count = input.available();  //number of symbols in a file
+            this.aByte = new byte[count];
+
+            while (input.available() > 0) {
+                input.read(aByte);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void writeTxt(StringBuffer aStr) {
+        try (BufferedWriter bwr = new BufferedWriter(new FileWriter(fileOut))) {
+            bwr.write(aStr.toString());
+            bwr.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
